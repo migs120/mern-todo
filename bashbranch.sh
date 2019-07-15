0f07157a245153953e6fec4171804e0fa4366fb2
migs120 <migs910@hotmail.com>
2019-07-15 15:47:42 -0700

    {}

--- bashbranch.sh
+++ bashbranch.sh
@@ -22,8 +22,8 @@
 # else
 # echo "You lost the game"
 # fi
-brnh=(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/') 
-git add . && git commit -m "{$message}" && git push origin brnh
+brnh={git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'} 
+git add . && git commit -m "{$message}" && git push origin $brnh
 #
 # num_dirs=3
 # pwd_symbol="..."
