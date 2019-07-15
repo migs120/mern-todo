58f4dc7b3044b394ef1b1de5e28955f6ed7a12c1
migs120 <migs910@hotmail.com>
2019-07-15 15:41:36 -0700

    {}

--- gitthis.sh
+++ gitthis.sh
@@ -23,7 +23,7 @@
 # echo "You lost the game"
 # fi
 
-git add . && git commit -m "{$message}" && git push origin "${ (git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/') }"
+git add . && git commit -m $MESSAGE && git push origin (git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/')
 #
 # num_dirs=3
 # pwd_symbol="..."
